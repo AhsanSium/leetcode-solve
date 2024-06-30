efd8a8a33a4c09525bc1d2e415b8971a73fbfbd2
AhsanSium <ahsansium28@gmail.com>
2024-06-30 23:28:59 +0600

    Neetcode RoadMap

--- leetCode 217 Contains Duplicate.cpp
+++ leetCode 217 Contains Duplicate.cpp
@@ -1,10 +1,29 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+
+//         for(int i = 0; i < nums.size() - 1; i++){
+//             if(nums[i] == nums[i+1]){
+//                 return true;
+//             }
+//         }
+
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        unordered_map <int, int> mp;
 
-        for(int i = 0; i < nums.size() - 1; i++){
-            if(nums[i] == nums[i+1]){
+        for(int i = 0; i < nums.size(); i++){
+            if(mp.find(nums[i]) == mp.end()){
+                // Not exists
+                mp[nums[i]] = 1;
+            }
+            else{
                 return true;
             }
         }
